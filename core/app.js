822b566c968dc6569cb785cbfe8860a5247abd94
ShangLiChun <shanglichun_0801@163.com>
2018-06-14 18:35:43 +0800

    添加lavas代码

---
+++ core/app.js
@@ -0,0 +1,35 @@
+/**
+ * @file entry
+ * @author ShangLiChun(shanglichun_0801@163.com)
+ */
+
+import Vue from 'vue';
+import Meta from 'vue-meta';
+
+import {createRouter} from '@/.lavas/router';
+import {createStore} from '@/.lavas/store';
+import AppComponent from './App.vue';
+import Vant from 'vant';
+import 'vant/lib/vant-css/index.css';
+import 'vant/lib/vant-css/icon-local.css';
+
+Vue.use(Meta);
+Vue.use(Vant);
+
+
+Vue.config.productionTip = false;
+
+export function createApp() {
+    let router = createRouter();
+    let store = createStore();
+    let App = Vue.extend({
+        router,
+        store,
+        ...AppComponent
+    });
+    return {App, router, store};
+}
+
+if (module.hot) {
+    module.hot.accept();
+}
---
+++ pages/Index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="page-index">
+    </div>
+</template>
+
+<script>
+function setState(store) {}
+
+export default {
+    name: 'index',
+    metaInfo: {
+        title: 'Home',
+        titleTemplate: '%s - Lavas',
+        meta: [
+            {name: 'keywords', content: 'lavas PWA'},
+            {name: 'description', content: '基于 Vue 的 PWA 解决方案，帮助开发者快速搭建 PWA 应用，解决接入 PWA 的各种问题'}
+        ]
+    },
+    async asyncData({store, route}) {
+        console.log(store)
+        console.log(route)
+        setState(store);
+    },
+    data(){
+        return {
+            active:0
+        }
+    },
+    created(){
+        this.watchActive()
+    },
+    methods:{
+        watchActive(){
+            console.log(this.active)
+            if(this.active === 0){
+                this.$router.push(`/home/${this.active}`)
+            }
+        }
+    },
+    watch:{
+        "active":"watchActive"
+    }
+};
+</script>
+
+<style lang="stylus" scoped>
+
+.page-index
+    display flex
+    justify-content center
+    align-items center
+    flex-direction column
+
+    h2
+        font-size 46px
+        font-weight 500
+        margin-bottom 0
+
+</style>
